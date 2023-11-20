a437646894701a7d9b20a7880d7b7f6f732fb61a
Hannes Preishuber <hannesp@ppedv.de>
2023-11-20 16:31:08 +0100

    REST HttpClient

---
+++ BlazorSchulungV8/Components/Pages/Modul4/ToDoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BlazorSchulungV8.Components.Pages.Modul4
+{
+    public class ToDoModel
+    {
+ 
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public bool completed { get; set; }
+    }
+
+}

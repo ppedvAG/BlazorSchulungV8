4831c8551f19a49fecbabdfb0d341909a8f0d500
Hannes Preishuber <hannesp@ppedv.de>
2023-11-21 11:05:23 +0100

    eigene InputText COmponent

---
+++ BlazorSchulungV8/Components/Pages/Modul5/Person.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorSchulungV8.Components.Pages.Modul5
+{
+    public class Person
+    {
+        [Required(ErrorMessage ="Da muss was rein")]
+        [StringLength(10,ErrorMessage ="max 10")]
+        public string Name { get; set; }
+
+
+        [Required(ErrorMessage = "Da muss was rein")]
+        [Range(150,190,ErrorMessage ="zwischen 150 und 190")]
+        public int  Gross { get; set; }
+
+        [Required(ErrorMessage = "Da muss was rein")]
+        public DateTime GebDat { get; set; }
+    }
+}
